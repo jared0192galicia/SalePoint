1921f70ce6c25dde6aed319b41c5c46ef37426d0
MATEBOOK-13\jared <jared0192galicia@gmail.com>
2024-01-22 08:22:55 -0600

    merge to origin

--- src/main/java/com/unsis/view/panel/GeneralSettings.java
+++ src/main/java/com/unsis/view/panel/GeneralSettings.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
  */
-package com.mycompany.cafe;
+package com.unsis.view.panel;
 
 import com.unsis.clases.Tools;
 import javax.swing.Icon;
