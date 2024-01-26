c9c98720fdf44d0db655a6153f59360ba0413b8c
Eliojustino <126814395+Eliojustino@users.noreply.github.com>
2024-01-26 16:53:47 -0600

    Update poblacion.sql
    Corregi la población de Sales y agregué la población de Product y Flavors

--- resources/poblacion.sql
+++ resources/poblacion.sql
@@ -1,103 +1,120 @@
-INSERT INTO "Employee" (numEmpleado, nombre, apellidoP, apellidoM, fechaNac, correo, telefono, fechaIng, estado, puesto)
-VALUES
-  (1001, 'Juan', 'Perez', 'Gomez', '1990-05-15', 'juan.perez@email.com', '123-456-7890', '2020-03-10', 'Activo', 'Desarrollador'),
-  (1002, 'Maria', 'Lopez', 'Rodriguez', '1985-09-22', 'maria.lopez@email.com', '987-654-3210', '2019-07-05', 'Activo', 'Diseñador'),
-  (1003, 'Pedro', 'Gonzalez', 'Fernandez', '1992-11-30', 'pedro.gonzalez@email.com', '555-555-5555', '2021-01-20', 'Activo', 'Analista'),
-  (1004, 'Laura', 'Ramirez', 'Diaz', '1988-04-18', 'laura.ramirez@email.com', '333-222-1111', '2018-11-15', 'Activo', 'Programador'),
-  (1005, 'Carlos', 'Mendoza', 'Castro', '1995-07-25', 'carlos.mendoza@email.com', '777-888-9999', '2017-06-10', 'Activo', 'Ingeniero'),
-  (1006, 'Isabel', 'Torres', 'Lopez', '1989-12-05', 'isabel.torres@email.com', '444-333-2222', '2019-03-28', 'Activo', 'Desarrollador'),
-  (1007, 'Fernando', 'Garcia', 'Hernandez', '1994-10-20', 'fernando.garcia@email.com', '666-777-8888', '2020-02-15', 'Activo', 'Analista'),
-  (1008, 'Luisa', 'Sanchez', 'Gutierrez', '1987-08-30', 'luisa.sanchez@email.com', '222-333-4444', '2016-09-12', 'Activo', 'Diseñador'),
-  (1009, 'Raul', 'Reyes', 'Perez', '1991-03-03', 'raul.reyes@email.com', '999-888-7777', '2021-07-07', 'Activo', 'Ingeniero'),
-  (1010, 'Ana', 'Martinez', 'Gomez', '1986-01-12', 'ana.martinez@email.com', '555-444-3333', '2015-05-01', 'Activo', 'Programador'),
-  (1011, 'Miguel', 'Fernandez', 'Lopez', '1993-06-08', 'miguel.fernandez@email.com', '111-222-3333', '2019-10-25', 'Activo', 'Desarrollador'),
-  (1012, 'Elena', 'Gutierrez', 'Hernandez', '1984-09-17', 'elena.gutierrez@email.com', '777-666-5555', '2018-03-15', 'Activo', 'Analista'),
-  (1013, 'David', 'Perez', 'Gomez', '1997-02-28', 'david.perez@email.com', '888-999-0000', '2020-08-20', 'Activo', 'Diseñador'),
-  (1014, 'Sofia', 'Rodriguez', 'Castro', '1990-07-10', 'sofia.rodriguez@email.com', '333-444-5555', '2017-11-30', 'Activo', 'Ingeniero'),
-  (1015, 'Roberto', 'Castillo', 'Lopez', '1985-12-20', 'roberto.castillo@email.com', '666-555-4444', '2016-04-05', 'Activo', 'Programador'),
-  (1016, 'Carmen', 'Lopez', 'Ramirez', '1996-04-05', 'carmen.lopez@email.com', '222-111-4444', '2019-12-10', 'Activo', 'Desarrollador'),
-  (1017, 'Javier', 'Gomez', 'Hernandez', '1993-09-15', 'javier.gomez@email.com', '999-888-2222', '2018-02-01', 'Activo', 'Analista'),
-  (1018, 'Olga', 'Hernandez', 'Perez', '1988-11-25', 'olga.hernandez@email.com', '444-333-1111', '2015-10-20', 'Activo', 'Diseñador'),
-  (1019, 'Andres', 'Martinez', 'Fernandez', '1990-03-01', 'andres.martinez@email.com', '111-222-7777', '2021-06-05', 'Activo', 'Ingeniero'),
-  (1020, 'Eva', 'Ramirez', 'Lopez', '1987-06-15', 'eva.ramirez@email.com', '555-666-9999', '2020-09-10', 'Activo', 'Programador'),
-  (1021, 'Hector', 'Gonzalez', 'Castro', '1994-01-08', 'hector.gonzalez@email.com', '777-888-1111', '2019-12-05', 'Activo', 'Desarrollador'),
-  (1022, 'Sara', 'Lopez', 'Hernandez', '1991-07-17', 'sara.lopez@email.com', '333-444-7777', '2018-05-30', 'Activo', 'Analista'),
-  (1023, 'Diego', 'Fernandez', 'Perez', '1995-02-28', 'diego.fernandez@email.com', '888-999-2222', '2017-10-01', 'Activo', 'Diseñador'),
-  (1024, 'Natalia', 'Perez', 'Gomez', '1993-08-20', 'natalia.perez@email.com', '222-111-3333', '2016-03-15', 'Activo', 'Ingeniero'),
-  (1025, 'Mario', 'Gutierrez', 'Ramirez', '1986-11-30', 'mario.gutierrez@email.com', '999-888-7777', '2021-04-05', 'Activo', 'Programador'),
-  (1026, 'Lourdes', 'Martinez', 'Lopez', '1989-05-15', 'lourdes.martinez@email.com', '555-666-2222', '2020-12-10', 'Activo', 'Desarrollador'),
-  (1027, 'Victor', 'Hernandez', 'Fernandez', '1997-03-10', 'victor.hernandez@email.com', '777-888-1111', '2019-11-05', 'Activo', 'Analista'),
-  (1028, 'Marta', 'Lopez', 'Perez', '1984-07-20', 'marta.lopez@email.com', '333-444-7777', '2018-04-30', 'Activo', 'Diseñador'),
-  (1029, 'Alejandro', 'Gomez', 'Gomez', '1992-12-01', 'alejandro.gomez@email.com', '888-999-2222', '2017-09-01', 'Activo', 'Ingeniero'),
-  (1030, 'Lucia', 'Fernandez', 'Ramirez', '1998-06-05', 'lucia.fernandez@email.com', '222-111-3333', '2016-02-15', 'Activo', 'Programador');
+CREATE Table "Company" (
+  id SERIAL PRIMARY KEY,
+  "name" VARCHAR NOT NULL,
+  logo VARCHAR,
+  descripcion VARCHAR
+);
 
-  INSERT INTO "Area" (nombre, descripcion)
-VALUES 
-('Recursos Humanos', 'Area para gestionar los empleados'),
-('Almacen', 'Area para la administración de productos'),
-('Ventas', 'Area de visualización de ventas'),
-('Ajustes', 'Area de ajustes del programa'),
-('Contabilidad', 'Area de manejo de efectivo');
+CREATE TABLE "Employee" (
+  id serial,
+  numEmpleado INT,
+  nombre VARCHAR,
+  apellidoP VARCHAR,
+  apellidoM VARCHAR,
+  fechaNac DATE,
+  correo VARCHAR,
+  telefono VARCHAR,
+  fechaIng DATE,
+  estado VARCHAR,
+  puesto VARCHAR,
+  PRIMARY KEY (id)
+);
+CREATE TABLE "Area" (
+  id serial,
+  nombre VARCHAR(25),
+  descripcion VARCHAR(75),
+  PRIMARY KEY (id)
+);
 
-  INSERT INTO "Section" (idArea, nombre, descripcion)
-VALUES 
-(1, 'Lista Empleados', 'Accede a la lista de empleados'),
-(1, 'Alta de Empleado', 'Registra un nuevo empleado'),
-(2, 'Alta de Producto', 'Registra un producto nuevo'),
-(2, 'Lista Productos', 'Accede a la lista de los productos'),
-(3, 'Ordenes entrantes', 'Accede a la lista de pedidos mas reciente'),
-(3, 'Punto de Venta', 'Reliza las ventas del dia'),
-(4, 'Ajustes del generales', 'Ajusta valores del programa'),
-(5, 'Corte de caja', 'Reliza tu corte de caja'),
-(5, 'Gastos', 'Registra los gastos permitidos');
-
-INSERT INTO "Account" (idempleado, numcuenta, "usuario", contrasena)
-VALUES 
-(1, 1, 'jGalicia', '$2a$12$r40Db2wKdLX75r/fS2pBxeEl0v.29j0/JPt/uVBENmL0lZsT9dxMK');
+CREATE TABLE "Section" (
+  id serial,
+  idArea INT,   
+  nombre VARCHAR(25),
+  descripcion VARCHAR(75),
+  PRIMARY KEY (id),
+  FOREIGN KEY (idArea) REFERENCES "Area"(id)
+);
 
 
-INSERT INTO "Access" (idcuenta, idseccion)
-VALUES (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (1, 7), (1, 8);
+CREATE TABLE "Account" (
+  id SERIAL,
+  numCuenta INT,
+  idEmpleado INT,
+  usuario VARCHAR(20),
+  contrasena VARCHAR(256),
+  PRIMARY KEY(id),
+  FOREIGN KEY (idEmpleado) REFERENCES "Employee"(id)
+);
 
+CREATE TABLE "Access" (
+  id serial,
+  idCuenta INT,
+  idSeccion INT,
+  PRIMARY KEY(id),
+  FOREIGN KEY (idCuenta) REFERENCES "Account"(id),
+  FOREIGN KEY (idSeccion) REFERENCES "Section"(id)
+);
+ 
+---------------------
 
--------------------------------
+CREATE TABLE "Supplier"(
+id serial,
+nombre VARCHAR,
+marca VARCHAR,
+PRIMARY KEY(id)
+);
 
-INSERT INTO
-  "Section" (idArea, nombre, descripcion)
-VALUES
-  (1, 'Editar Empleados', 'Modifica los empleados');
+CREATE TABLE "Product"(
+  id serial,
+  codigoBarra VARCHAR UNIQUE,
+  nombre varchar,
+  precioCom float,
+  precioVenta float,
+  tipo VARCHAR,
+  descripcion VARCHAR(25),
+  numProducto INT,
+  estado VARCHAR(4),
+  disponible int,
+  variante VARCHAR,
+  PRIMARY KEY(id)
+);
 
-INSERT INTO
-  "Section" (idArea, nombre, descripcion)
-VALUES
-  (2, 'Editar Productos', 'Modifica los productos');
+CREATE TABLE "Flavors" (
+  id serial PRIMARY KEY,
+  idProducto INT,
+  sabor VARCHAR(50),
+  FOREIGN KEY (idProducto) REFERENCES "Product" (id)
+);
 
--- Población de la tabla Supplier
-INSERT INTO "Supplier" (nombre, marca) VALUES
-('Proveedor A', 'Marca A'),
-('Proveedor B', 'Marca B'),
-('Proveedor C', 'Marca C'),
-('Proveedor D', 'Marca D'),
-('Proveedor E', 'Marca E');
+CREATE TABLE "Sales"(
+  id serial,
+  idVenta int,
+  idEmpleado int,
+  idProducto VARCHAR,
+  tipoOrden VARCHAR CHECK (tipoOrden IN ('Para llevar', 'Normal')),
+  comentarios VARCHAR,
+  nombreComprador VARCHAR,
+  codigoBarra VARCHAR,
+  PRIMARY KEY(id),
+  FOREIGN KEY (idEmpleado) REFERENCES "Employee"(id),
+  FOREIGN KEY (idProducto) REFERENCES "Product"(codigoBarra)
+);
 
--- Población de la tabla Sales
-INSERT INTO "Sales" (idEmpleado, sabor, tipo_orden, comentarios, nombre_comprador, codigo_barra) VALUES
-(1, 'Chocolate', 'Online', 'Entrega rápida', 'Cliente 1', '123456789012'),
-(2, 'Fresa', 'In-Store', 'Sin comentarios', 'Cliente 2', '234567890123'),
-(3, 'Vainilla', 'Delivery', 'Producto defectuoso', 'Cliente 3', '345678901234'),
-(1, 'Café', 'Online', 'Entrega demorada', 'Cliente 4', '456789012345'),
-(2, 'Mango', 'In-Store', 'Buen servicio', 'Cliente 5', '567890123456');
+ALTER TABLE
+  "Account"
+ADD
+  COLUMN fotoperfil VARCHAR;
 
--- Población de la tabla Product
-INSERT INTO "Product" (nombre, precioCom, precioVenta, codigoBarra, tipo, descripcion, numProducto, estado, disponible, variante) VALUES
-('Producto 1', 10.5, 15.99, '111111', 'Postre', 'Descripción 1', 1001, 'Activo', 20, 'Variante A'),
-('Producto 2', 8.75, 12.49, '222222', 'Helado', 'Descripción 2', 1002, 'Activo', 15, 'Variante B'),
-('Producto 3', 12.0, 19.99, '333333', 'Pastel', 'Descripción 3', 1003, 'Inactivo', 0, 'Variante C'),
-('Producto 4', 15.25, 24.99, '444444', 'Gelato', 'Descripción 4', 1004, 'Activo', 30, 'Variante D'),
-('Producto 5', 9.99, 16.99, '555555', 'Cupcake', 'Descripción 5', 1005, 'Activo', 25, 'Variante E');
+ALTER TABLE
+  "Account"
+ALTER COLUMN
+  fotoperfil
+SET
+  DEFAULT '/profileDefault.png' NOTNULL;
 
-
---------------------------------
-
-INSERT INTO "Company" ("name", descripcion, logo) VALUES ('Tienda', 'nose', './');
-
+UPDATE
+  "Account"
+SET
+  fotoperfil = '/profileDefault.png'
+WHERE
+  fotoperfil IS NULL;
